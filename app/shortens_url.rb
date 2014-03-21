77d5b95208d5ab142d184bfbae2595c7537881de
Lukasz Rozek <lukasz.rozek84@gmail.com>
2014-03-21 14:50:15 +0100

    shorter urls

--- app/shortens_url.rb
+++ app/shortens_url.rb
@@ -1,6 +1,6 @@
 class ShortensUrl
 	def shorten(base_url, full_url)
-		hash = SecureRandom.urlsafe_base64(16, true)
+		hash = SecureRandom.urlsafe_base64(3, true)
 		shortend_url = "#{base_url}/#{hash}"
 	end
 end
