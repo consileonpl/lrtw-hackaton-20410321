e73d50f8b637184f92b92ae54147e6d05d33de1f
Lukasz Rozek <lukasz.rozek84@gmail.com>
2014-03-21 13:44:50 +0100

    removed debug stuff

--- app/hackaton.rb
+++ app/hackaton.rb
@@ -1,15 +1,13 @@
 require 'sinatra'
 require_relative './shortens_url'
 require 'sinatra/json'
-require 'pry'
 
 get '/' do
   'Hello world!'
 end
 
 post '/' do
-	binding.pry
 	shortens_url_service = ShortensUrl.new
 	shorten_url = shortens_url_service.shorten(params[:url])
-	{shortend_url: shorten_url} 
+	json shortend_url: shorten_url
 end
